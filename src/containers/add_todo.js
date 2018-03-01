9f208034a7fbfc1252d5ec4866d19143a56983c5
Windows <gersoncalienes@gmail.com>
2018-03-01 12:44:40 -0500

    Set up basic functionality of AddTodo container

--- src/containers/add_todo.js
+++ src/containers/add_todo.js
@@ -6,13 +6,34 @@ class AddTodo extends Component {
     this.state = {
       todoTitle: ''
     };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+    this.setState({ todoTitle: event.target.value }); //"this" needs to bind!!!!
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    //Clear input after sumbitting
+    this.setState({ todoTitle: '' });
   }
 
   render() {
     return (
       <div>
-        <form action="">
-          <input type="text" />
+        <form onSubmit={this.onFormSubmit}>
+          <input
+            type="text"
+            autoFocus
+            placeholder="New"
+            className="form-control"
+            value={this.state.todoTitle}
+            onChange={this.onInputChange}
+          />
           <button>Submit</button>
         </form>
       </div>
