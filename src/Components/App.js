58e5c2d10e8290e2c1f7f31357650abfa34b0f26
Windows <gersoncalienes@gmail.com>
2018-03-01 17:00:41 -0500

    Starting css globals

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>To-Do</h2>
+        <h2 className="main-border">To-Do</h2>
         <AppContainer />
       </div>
     );
