a2a92220944ea67851042fa3561120323bb9ef33
Windows <gersoncalienes@gmail.com>
2018-03-01 22:27:58 -0500

    Added new favicon and image from Genius Plaza

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,7 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2 className="main-border border-padding">To-Do</h2>
+        <div className="app-header">
+          <img src={require('../styles/assets/GP-sign2.png')} alt="" />
+          <h2 className="main-border border-padding">To-Do</h2>
+        </div>
         <AppContainer />
       </div>
     );
